066c0144c526f4c1a2db6a1e7ee962b0da401ecb
muhammed sinan <muhammedsinanvavoor@gamil.com>
2025-10-09 16:30:39 +0530

    change all letters to upper case

---
+++ node-study/upper-case.js
@@ -0,0 +1,13 @@
+// change the  case letters to upper case 
+
+const toUpper = require('to-upper');
+
+let number=123
+
+let name= `my name is sina and my number is ${number} __test`
+
+
+console.log(toUpper(name));
+
+
+// output (MY NAME IS SINA AND MY NUMBER IS 123 __TEST)
